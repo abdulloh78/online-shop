2da605127e03e71080d18b0c0d966b41ac57d070
abdulloh78 <134940575+abdulloh78@users.noreply.github.com>
2023-09-24 10:03:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ for (let i = 0; i < add_to_cart_buttons.length; i += 1){
         cart_items_count.innerHTML = +cart_items_count.innerHTML  + 1
     })
 }
+let block = document.querySelector('.block')
+document.addEventListener('mousemove', function(e) {
+    let dx = e.pageX - window.innerWidth / 2
+    let dy = e.pageY - window.innerHeight / 2
+    let angleX = 20 * dx / window.innerWidth / 2
+    let angleY = 20 * dy / window.innerHeight / 2
+    block.style.transform = `rotateX(${-angleY}deg) rotateY(${angleX}deg)`
+})
