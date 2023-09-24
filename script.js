433074707185e95146053b3b57946e5a0666a342
abdulloh78 <134940575+abdulloh78@users.noreply.github.com>
2023-09-24 09:29:29 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
-let Kategoriya = document.querySelector('.Kategoriya')
-let answers = document.querySelectorAll('.korzinka')
-
-let cookie = false
-let cookies = document.cookie.split('; ')
-
-for (let i=0; i < cookies.length; i +=1){
-    if (cookies[i].split('=')[0]=='products'){
-        
-        break
-    }
+let cart_items_count = document.querySelector('.cart-amount')
+let add_to_cart_buttons = document.querySelectorAll('.btn-12')
+for (let i = 0; i < add_to_cart_buttons.length; i += 1){
+    add_to_cart_buttons[i].addEventListener('click', function() {
+        cart_items_count.innerHTML = +cart_items_count.innerHTML  + 1
+    })
 }
-if (cookie){
-    
-container_main.style.display = "none"
-
-
-btn_start.addEventListener('click', function(){
-    container_start.style.display = 'none'
-    container_main.style.display = 'flex'
-})
